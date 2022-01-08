fffe3d6003800f2861c5412bd178f023b7cdcbbe
bgoodwin91006-1 <66847957+bgoodwin91006-1@users.noreply.github.com>
2022-01-08 10:00:27 -0800

    Update close-container.sh

--- close-container.sh
+++ close-container.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-if  [ -a /home/$SUDO_USER/Rootfs/dev ] #minimal sanity check
+if  [ -d /home/$SUDO_USER/Rootfs/dev ] #minimal sanity check
     then
     umount /home/$SUDO_USER/Rootfs/dev  
-    umount /home/$SUDO_USER/Rootfs/opt
-    umount /home/$SUDO_USER/Rootfs/tmp 
+    umount /home/$SUDO_USER/Rootfs/opt 
     umount /home/$SUDO_USER/Rootfs/bin 
-    umount /home/$SUDO_USER/Rootfs/lib 
-    umount /home/$SUDO_USER/Rootfs/lib64 
-    umount /home/$SUDO_USER/Rootfs/proc 
+    umount /home/$SUDO_USER/Rootfs/lib
     umount /home/$SUDO_USER/Rootfs/sys 
     umount /home/$SUDO_USER/Rootfs/run 
     umount /home/$SUDO_USER/Rootfs/usr 
     umount /home/$SUDO_USER/Rootfs/var
     umount /home/$SUDO_USER/Rootfs/etc
+    umount /home/$SUDO_USER/Rootfs/sbin
+    umount /home/$SUDO_USER/Rootfs/lib64
+#   umount /home/$SUDO_USER/Rootfs/proc # Mounted and unmounted within the container
+#   umount /home/$SUDO_USER/Rootfs/tmp  # Not normally mounted to avoid bleeding
+#                                       # from container to host.
 else
     echo "home/$SUDO_USER/Rootfs does not appear to contain a filesystem."
+    exit
 fi
-if [-a /home/$SUDO_USER/Rootfs/lib32 ]
+
+if [ -d /home/$SUDO_USER/Rootfs/lib32 ]
     then
     umount /home/$SUDO_USER/Rootfs/lib32
 fi
-
-#example for any non-rootfs devices mounted to the container in the 'open' scriopt
-#if  [ -d /home/$SUDO_USER/Rootfs/Elements ]
+# Example for any non-rootfs devices mounted to the container in the 'open' scriopt
+#if  [ -d home/$SUDO_USER/Rootfs/Elements ]
 #   then
 #   umount /home/$SUDO_USER/Rootfs/Elements 
 #fi
